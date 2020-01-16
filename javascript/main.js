a0ecda6c9b8cb96de0d355235501ba4129dd23ae
Mattia Pedone <mattiapedone93@gmail.com>
2020-01-16 17:30:52 +0100

    piccole modifiche all'esercizio

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,7 @@
 var hamburgerMenuBtn = $('.header-right > a');
 var hamburger_menu = $('.hamburger-menu');
 var closeMenu = $('.close');
+
 hamburgerMenuBtn.click(
   function() {
     hamburger_menu.show(1000);
